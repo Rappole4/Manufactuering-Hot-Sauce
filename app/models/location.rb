de174fe44084e8ff867d73314233417828ffc93f
Robert <rappole4@yahoo.com>
2017-03-02 11:13:49 -0500

    shows the proper city and state on show page

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ApplicationRecord
+	has_many :sauces, foreign_key: :origin_id
+	def fullorigin
+		"#{self.city}, #{self.state}"
+	end
 end
--- app/models/sauce.rb
+++ app/models/sauce.rb
@@ -1,6 +1,6 @@
 class Sauce < ApplicationRecord
   validates :image, presence: true
-
+  belongs_to :origin, class_name:"Location"
   has_attached_file :image, styles: { :small => "300x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
