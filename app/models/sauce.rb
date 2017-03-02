ba0e48d0345fb7abd1d3932ce4c134c816d94062
Eric Sui <zanzanegi@gmail.com>
2017-03-02 11:09:46 -0500

    trying to make it so that only owners of the sauce can edit it

--- app/models/sauce.rb
+++ app/models/sauce.rb
@@ -1,5 +1,6 @@
 class Sauce < ApplicationRecord
-  validates :image, presence: true
+  validates :image, :user_id, presence: true
+  belongs_to :user
 
   has_attached_file :image, styles: { :small => "300x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
